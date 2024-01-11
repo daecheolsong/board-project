0ff0f585f099ba84436e0323564fc44d94883cec
daecheolsong <eocjf8905@naver.com>
2024-01-11 16:29:18 +0900

    #17 - 게시글 관련 뷰 테스트 작성
    우선 어떤 경로로 뷰를 열어줄지 명세를 추출.
    기존 테스트는 아직 구현 전이므로 기능 구현중인 유닛테스트는 `@Disabled`처리.

---
+++ src/main/java/com/example/boardproject/controller/ArticleController.java
@@ -0,0 +1,14 @@
+package com.example.boardproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author daecheol song
+ * @since 1.0
+ */
+@Controller
+@RequestMapping("/article")
+public class ArticleController {
+
+}
