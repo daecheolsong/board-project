cddacfb8c55c3ca605f0b9b1e8cb6af7951ae759
daecheolsong <eocjf8905@naver.com>
2024-01-11 13:17:35 +0900

    #2 - data rest 설정
    article, article_comment 의 json api를 자동으로 restful 하게 만들게 설정

--- src/main/java/com/example/boardproject/repository/ArticleRepository.java
+++ src/main/java/com/example/boardproject/repository/ArticleRepository.java
@@ -2,10 +2,12 @@ package com.example.boardproject.repository;
 
 import com.example.boardproject.domain.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /**
  * @author daecheol song
  * @since 1.0
  */
+@RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
 }
