edb973ff759fc2ccbf4b6ae84adc6cd56b8f4c1a
daecheolsong <eocjf8905@naver.com>
2024-01-14 14:58:18 +0900

    #31 - 게시판 검색 기능 구현
    * 게시판 검색을 위한 뷰 수정
    * SearchType @RequestParamBinding 을 위해 String 으로 받는 of() 팩터리 메서드 생성
    * 검색 기능 구현에 따른 테스트 수정

--- src/main/java/com/example/boardproject/domain/type/SearchType.java
+++ src/main/java/com/example/boardproject/domain/type/SearchType.java
@@ -1,9 +1,29 @@
 package com.example.boardproject.domain.type;
 
+import lombok.Getter;
+
 /**
  * @author daecheol song
  * @since 1.0
  */
 public enum SearchType {
-    TITLE, CONTENT, ID, NICKNAME, HASHTAG
+
+    TITLE("제목"),
+    CONTENT("본문"),
+    ID("유저 ID"),
+    NICKNAME("닉네임"),
+    HASHTAG("해시태그"),
+    NONE("선택 안함");
+
+    @Getter private final String description;
+
+    SearchType(String description) {
+        this.description = description;
+    }
+
+    public static SearchType of(String value) {
+        for(SearchType v : values())
+            if(v.getDescription().equalsIgnoreCase(value)) return v;
+        return SearchType.NONE;
+    }
 }
