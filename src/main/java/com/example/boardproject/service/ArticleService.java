5d5e30eed16a1ce1dd1f8e1e28577409f418d9a6
daecheolsong <eocjf8905@naver.com>
2024-01-14 09:59:02 +0900

    #28 - 게시글 페이지네이션 구현
    이전글, 다음글 만 구현.

--- src/main/java/com/example/boardproject/service/ArticleService.java
+++ src/main/java/com/example/boardproject/service/ArticleService.java
@@ -72,4 +72,8 @@ public class ArticleService {
     public void deleteArticle(Long articleId) {
         articleRepository.deleteById(articleId);
     }
+
+    public long getArticleCount() {
+        return articleRepository.count();
+    }
 }
